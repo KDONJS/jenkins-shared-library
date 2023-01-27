d5a8214d53ac9e29262932bf343849f0291ad09a
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2023-01-27 11:26:43 -0500

    test muestreo de mensajes - 9

--- src/sharedLibrary/toolsJenkisn.groovy
+++ src/sharedLibrary/toolsJenkisn.groovy
@@ -13,18 +13,8 @@ class toolsJenkisn implements Serializable{
     }
 
     def call(String name) {
-        this.printMessage("Hola ${name}")
-        this.printMessage("Nombre del job: ${steps.env.JOB_NAME}")
-        //ejecutar comando de linux en jenkins
-        steps.sh "cd .. && cd pipelineGroovy@tmp && ls -la"
-
-        //mostrar el directorio donde se encuentra la libreria compartida
-        this.printMessage("Directorio de la libreria compartida: ${steps.env.WORKSPACE}")
-        //ingresar a un directorio
-        steps.dir("${steps.env.WORKSPACE}/pipelineGroovy@tmp") {
-            //ejecutar comando de linux en jenkins
-            steps.sh "cd .. && ls -la"
-        }
+        //ejecutar archivo sh de la del proyecto de jenkins
+        steps.sh "sh /scripts/taxonomia.sh ${name}"
     }
 
     //metodo copiado de archivos de la libreria shared-library al workspace de jenkins 
