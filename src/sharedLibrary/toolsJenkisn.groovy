3e7df2833c99c730d225c37c9d4d3a7697f17a88
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2023-01-30 15:21:26 -0500

    test - v10

--- src/sharedLibrary/toolsJenkisn.groovy
+++ src/sharedLibrary/toolsJenkisn.groovy
@@ -21,7 +21,7 @@ class toolsJenkisn implements Serializable{
     }
 
     def call(String name) {
-        String taxonomia = steps.libraryResource(encoding: 'UTF-8', resource: 'scripts/taxonomia.sh')
+        String taxonomia = steps.libraryResource(encoding: 'UTF-8', resource: 'dataLake/taxonomia.sh')
 
         steps.dir("${steps.env.WORKSPACE}/pipelineGroovy@tmp") {
             steps.writeFile file: 'taxonomia.sh', text: taxonomia
