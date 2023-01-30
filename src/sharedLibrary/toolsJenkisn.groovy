d1aa6abaf7342571d076118d210fa6c9250baef9
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2023-01-30 14:55:41 -0500

    test - v4

--- src/sharedLibrary/toolsJenkisn.groovy
+++ src/sharedLibrary/toolsJenkisn.groovy
@@ -1,5 +1,11 @@
 package sharedLibrary
 
+import java.io.file
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.zip.ZipOutputStream
+
+
 class toolsJenkisn implements Serializable{
 
     def steps
@@ -13,13 +19,47 @@ class toolsJenkisn implements Serializable{
     }
 
     def call(String name) {
-        String taxonomia = steps.libraryResource(encoding: 'UTF-8', resource: 'scripts/taxonomia.sh')
+        // String taxonomia = steps.libraryResource(encoding: 'UTF-8', resource: 'scripts/taxonomia.sh')
+
+        // steps.dir("${steps.env.WORKSPACE}/pipelineGroovy@tmp") {
+        //     steps.writeFile file: 'taxonomia.sh', text: taxonomia
+        //     steps.sh "chmod +x taxonomia.sh"
+        //     steps.sh "./taxonomia.sh ${name}"
+        // }
+
+        def fileScripZip = new File(libraryResource('scripts').toString())
+        def zipFile = new File("${steps.env.WORKSPACE}/pipelineGroovy@tmp", "scripts.zip")
+
+        zipFile.delete()
+
+        def fos = new FileOutputStream(zipFile)
+        def zos = new ZipOutputStream(fos)
+
+        fileScripZip.eachFileRecurse{ file ->
+             if (!file.isDirectory()){
+                def entry = new ZipEntry(file.name)
+                zos.putNextEntry(entry)
+                def fis = new FileInputStream(file)
+                def buffer = new byte[1024]
+                int length
+                while ((length = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, length)
+                }
+                zos.closeEntry()
+                fis.close()
+             }
+        }
+
+        zos.close()
+        fos.close()
 
         steps.dir("${steps.env.WORKSPACE}/pipelineGroovy@tmp") {
-            steps.writeFile file: 'taxonomia.sh', text: taxonomia
-            steps.sh "chmod +x taxonomia.sh"
-            steps.sh "./taxonomia.sh ${name}"
+            steps.sh "unzip scripts.zip"
+            steps.sh "chmod +x scripts/taxonomia.sh"
+            steps.sh "./scripts/taxonomia.sh ${name}"
         }
+
+
     }
 
     //metodo copiado de archivos de la libreria shared-library al workspace de jenkins 
