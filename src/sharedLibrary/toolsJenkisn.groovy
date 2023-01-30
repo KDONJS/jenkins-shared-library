62d6f4387a4b5fab139e9a2070386946c4d28d2e
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2023-01-30 15:18:53 -0500

    test - v8

--- src/sharedLibrary/toolsJenkisn.groovy
+++ src/sharedLibrary/toolsJenkisn.groovy
@@ -29,7 +29,7 @@ class toolsJenkisn implements Serializable{
         //     steps.sh "./taxonomia.sh ${name}"
         // }
 
-        def fileScripZip = new File(libraryResource( resource: 'scripts').toString())
+        def fileScripZip = new File(libraryResource('dataLake').toString())
         def zipFile = new File("${steps.env.WORKSPACE}/pipelineGroovy@tmp", "scripts.zip")
 
         zipFile.delete()
