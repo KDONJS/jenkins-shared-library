02cfb47bbeb3f5cd087c35ee43023f9cd2620e56
KDONJS <yorlinssj@hotmail.com>
2024-01-07 13:35:32 -0500

    ssss

--- src/sharedLibrary/toolsJenkisn.groovy
+++ src/sharedLibrary/toolsJenkisn.groovy
@@ -6,14 +6,16 @@ class toolsJenkisn implements Serializable{
 
     def steps
     def script
+    def env
 
     toolsJenkisn(steps, script) {
         this.steps = steps
         this.script = script
+        this.env = env
     }
 
     def printMessage(String message) {
-        steps.script.echo "[KDON-DevSecOps]: ${message}"
+        steps.echo "[KDON-DevSecOps]: ${message}"
     }
 
     def callToActions(String name) {
