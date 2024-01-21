1948fd999650e7ea67b919eb43c24a28df929729
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2024-01-21 13:27:46 -0500

    Update toolsJenkisn.groovy

--- src/sharedLibrary/toolsJenkisn.groovy
+++ src/sharedLibrary/toolsJenkisn.groovy
@@ -19,6 +19,7 @@ class toolsJenkisn implements Serializable{
 
     def callToActions(String name) {
         steps.sh "gcloud components list"
+        steps.sh "apt-get install -y kubectl"
         this.printMessage("${script.env.WORKSPACE}")
     }
 
